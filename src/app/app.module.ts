c205282cc47e8d9923bdc31b079be3c1a6a9f904
AndreaBellone123 <andrezombiekiller646@gmail.com>
2020-05-24 13:07:28 +0200

    sad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,38 @@ import { AppRoutingModule } from './app-routing.module';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment';
 
+/* // Imports the Google Cloud client library
+const {Storage} = require('@google-cloud/storage');
+ 
+// Creates a client
+const storage = new Storage();
 
+// Creates a client from a Google service account key.
+// const storage = new Storage({keyFilename: "key.json"});
 
+const bucketName = 'ionic-form-ui';
+ 
+async function createBucket() {
+  // Creates the new bucket
+  await storage.createBucket(bucketName);
+  console.log(`Bucket ${bucketName} created.`);
+}
+ 
+createBucket().catch(console.error);
+
+*/
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [AngularFirestoreModule,AngularFireStorageModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebase)],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy ,
+      }
   ],
   bootstrap: [AppComponent]
 })
